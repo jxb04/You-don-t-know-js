e29c85fa6faf887a3a9e9df33492bd01950fbb23
Joel Bernasky <joel_bernasky@hotmail.com>
2017-10-09 23:06:07 -0400

    end of chapter exercise

---
+++ Up and Going/exercise1.js
@@ -0,0 +1,16 @@
+var bankAccount = 599.95;
+
+const TAX_RATE = 0.08;
+
+const ACCESSORY_PRICE = 9.99;
+const PHONE_PRICE = 199.99;
+
+const SPENDING_THRESHOLD;
+
+function calculateFinalPurchaseAmount(amt) {
+  amt = amt + (amt*TAX_RATE);
+
+  retrun amt;
+}
+
+console.log ("Purchase amount  $" + total);
